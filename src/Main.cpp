7698e3a51dc97a9ba1d9ef9acecd11ab599f49f6
banelle <banelle@users.noreply.github.com>
2019-06-28 22:41:33 +0100

    Shonky attempt at threading the processing code

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,11 +2,14 @@
 #include <sstream>
 #include <chrono>
 #include <cmath>
+#include <thread>
 #include "Process.hpp"
 #include "Video.hpp"
 
 using namespace std;
 using namespace DerperView;
+
+const int TOTAL_THREADS = 4;
 
 void Go(string filename)
 {
@@ -23,16 +26,19 @@ void Go(string filename)
     int64_t encodedFrameCount = 0;
     int frameBufferSize = -1;
 
-    vector<unsigned char> data;
-    vector<unsigned char> derperviewedData;
+    AVFrame *outputFrame = nullptr;
+    vector<unsigned char> data[TOTAL_THREADS];
+    vector<unsigned char> derperviewedData[TOTAL_THREADS];
     int targetWidth = 0;
-
+    int pixelFormat = -1;
+    int threadIndex = -1;
+    thread threads[TOTAL_THREADS];
+    
     cout << "--------------------------------------------------------------------" <<  endl;
 
     auto frame = input.GetNextFrame();
     while (frame != nullptr)
     {
-        AVFrame *outputFrame = nullptr;
         if (frame->width == 0) // Audio - stream it through
         {
             output.WriteNextFrame(frame);
@@ -40,48 +46,89 @@ void Go(string filename)
         else // Video, stretch that bad boy.
         {
             // If this is the first video frame we've seen, set our buffers and stuff up
-            if (data.size() == 0)
+            if (threadIndex == -1)
             {
+                pixelFormat = frame->format;
+
                 if (frame->width * 3 / 4 != frame->height) // Funky looking maths to keep this int
                 {
                     cerr << "Source not in 4:3 aspect ratio" << endl;
                     return;
                 }
-                if (frame != nullptr)
+
+                frameBufferSize = av_image_get_buffer_size(static_cast<AVPixelFormat>(frame->format), frame->width, frame->height, 1);
+                targetWidth = frame->width * 4 / 3;
+                for (int i = 0; i < TOTAL_THREADS; i++)
                 {
-                    frameBufferSize = av_image_get_buffer_size(static_cast<AVPixelFormat>(frame->format), frame->width, frame->height, 1);
-                    data.resize(frameBufferSize);
-                    targetWidth = frame->width * 4 / 3;
-                    derperviewedData.resize(static_cast<int>(frame->height * targetWidth * 1.5));
+                    data[i].resize(frameBufferSize);
+                    derperviewedData[i].resize(static_cast<int>(frame->height * targetWidth * 1.5));
                 }
+
+                threadIndex = 0;
             }
 
             // Copy data into a contiguous buffer we can mess with
-            auto copyResult = av_image_copy_to_buffer(data.data(), frameBufferSize, frame->data, frame->linesize, static_cast<AVPixelFormat>(frame->format), frame->width, frame->height, 1);
+            auto copyResult = av_image_copy_to_buffer(data[threadIndex].data(), frameBufferSize, frame->data, frame->linesize, static_cast<AVPixelFormat>(frame->format), frame->width, frame->height, 1);
 
             // Perform the stretchy stuff
-            ProcessFrameYuv(frame->width, frame->height, data, derperviewedData);
+            threads[threadIndex] = thread(ProcessFrameYuv, frame->width, frame->height, ref(data[threadIndex]), ref(derperviewedData[threadIndex]));
+            threadIndex ++;
 
-            // Put new data into an AVFrame
-            outputFrame = av_frame_alloc();
-            outputFrame->width = targetWidth;
-            outputFrame->height = frame->height;
-            outputFrame->format = frame->format;
-            outputFrame->pts = frame->pts;
-            av_image_fill_arrays(outputFrame->data, outputFrame->linesize, reinterpret_cast<uint8_t *>(derperviewedData.data()), static_cast<AVPixelFormat>(outputFrame->format), outputFrame->width, outputFrame->height, 1);
+            if (threadIndex >= TOTAL_THREADS)
+            {
+                for (int i = 0; i < TOTAL_THREADS; i ++)
+                    threads[i].join();
 
-            encodedFrameCount += output.WriteNextFrame(outputFrame);
+                for (int i = 0; i < TOTAL_THREADS; i++)
+                {
+                    // Put new data into an AVFrame
+                    outputFrame = av_frame_alloc();
+                    outputFrame->width = targetWidth;
+                    outputFrame->height = frame->height;
+                    outputFrame->format = frame->format;
+                    outputFrame->pts = frameCount;
+                    av_image_fill_arrays(outputFrame->data, outputFrame->linesize, reinterpret_cast<uint8_t *>(derperviewedData[i].data()), static_cast<AVPixelFormat>(outputFrame->format), outputFrame->width, outputFrame->height, 1);
 
-            av_frame_free(&outputFrame);
-            frameCount++;
+                    encodedFrameCount += output.WriteNextFrame(outputFrame);
 
-            if (frameCount % percentageMarker == 0)
-                cout << " " << ceil(static_cast<float>(frameCount) * 100 / videoInfo.totalFrames) << "% ";
-            else if (frameCount % 5 == 0)
-                cout << ".";
+                    av_frame_free(&outputFrame);
+                    frameCount++;
+
+                    if (frameCount % percentageMarker == 0)
+                        cout << " " << ceil(static_cast<float>(frameCount) * 100 / videoInfo.totalFrames) << "% ";
+                    else if (frameCount % 5 == 0)
+                        cout << ".";
+                }
+
+                threadIndex = 0;
+            }
         }
 
         frame = input.GetNextFrame();
+    }
+
+    // Clear left-over frames out of the thread buffer
+    for (int i = 0; i < threadIndex; i++)
+        threads[i].join();
+
+    for (int i = 0; i < threadIndex; i++)
+    {
+        outputFrame = av_frame_alloc();
+        outputFrame->width = targetWidth;
+        outputFrame->height = videoInfo.height;
+        outputFrame->format = pixelFormat;
+        outputFrame->pts = frameCount;
+        av_image_fill_arrays(outputFrame->data, outputFrame->linesize, reinterpret_cast<uint8_t *>(derperviewedData[i].data()), static_cast<AVPixelFormat>(outputFrame->format), outputFrame->width, outputFrame->height, 1);
+
+        encodedFrameCount += output.WriteNextFrame(outputFrame);
+
+        av_frame_free(&outputFrame);
+        frameCount++;
+
+        if (frameCount % percentageMarker == 0)
+            cout << " " << ceil(static_cast<float>(frameCount) * 100 / videoInfo.totalFrames) << "% ";
+        else if (frameCount % 5 == 0)
+            cout << ".";
     }
 
     output.Flush();
