157237f892b25492703936cde524a6b570b3a08a
banelle <banelle@users.noreply.github.com>
2023-03-22 08:52:30 +0000

    Yanked Travis stuff. Some directory renaming

--- ci-scripts/package-windows.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pushd ../bin/Release
-cp ../../lib/ffmpeg-shared-windows/bin/swresample-3.dll ./
-cp ../../lib/ffmpeg-shared-windows/bin/avcodec-58.dll ./
-cp ../../lib/ffmpeg-shared-windows/bin/avformat-58.dll ./
-cp ../../lib/ffmpeg-shared-windows/bin/avutil-56.dll ./
-7z a ../../deploy/derperview-win64.zip derperview.exe swresample-3.dll avcodec-58.dll avformat-58.dll avutil-56.dll
-popd
